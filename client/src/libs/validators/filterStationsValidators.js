09a6501bc888c541436fc4772e11200669673fc8
javiercavlop <javiercavlop@gmail.com>
2023-05-04 14:06:24 +0200

    FIX: FILTERS VALIDATIONS

--- client/src/libs/validators/filterStationsValidators.js
+++ client/src/libs/validators/filterStationsValidators.js
@@ -3,37 +3,37 @@ export const filterStationsValidators = {
     validStatus: {
         validate: (value) => {
             let validStatus = ['Abierta', 'Cerrada', 'Cualquiera'];
-            return validStatus.includes(value);
+            return !value || validStatus.includes(value);
         },
         message: "El campo debe ser un estado válido",
     },
     notFutureDate: {
         validate: (value) => {
-            return new Date(value) < new Date();
+            return !value || new Date(value) < new Date();
         },
         message: "La fecha no puede ser posterior a la actual"
     },
     notNegativeNumber: {
         validate: (value) => {
-            return value >= 0;
+            return !value || value >= 0;
         },
         message: "El campo no puede ser un número negativo"
     },
     notPastDate: {
         validate: (value) => {
-            return new Date(value) >= new Date();
+            return !value || new Date(value) >= new Date();
         },
         message: "La fecha no puede ser anterior a la actual"
     }, 
     notMoreThanOneWeek: {
         validate: (value) => {
-            return new Date(value) < new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+            return !value || new Date(value) < new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
         },
         message: "La fecha no puede ser posterior a una semana"
     },
     positiveNumber: {
         validate: (value) => {
-            return value > 0;
+            return !value || value > 0;
         },
         message: "El campo debe ser un número positivo"
     }
